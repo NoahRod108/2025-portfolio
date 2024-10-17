9a81f23c7a9c3c3b15f45545708b3f13052f248b
Noah Rodriguez <nrodrig3@valpo.edu>
2024-10-17 16:57:59 -0500

    first commit

---
+++ sections/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-1 border border-white/15 rounded-full bg-white/10">
+        <a href="#" className="nav-item">
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          About
+        </a>
+        <a href="#" className="nav-item bg-white text-[--background]">
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
