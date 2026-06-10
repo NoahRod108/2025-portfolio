002a1c3fe0b64a2ecd0b2d85a7abbeb897f3357b
NoahRod108 <noah.rodriguez.portfolio@gmail.com>
2026-06-10 15:00:19 -0500

    Removed backend project

--- constants/index.js
+++ constants/index.js
@@ -28,30 +28,30 @@ export const projects = [
     githubLink: "https://github.com/NoahRod108/next-js-tutorial",
     tags: ["Next.js", "Tailwind", "MongoDB", "NextAuth"],
   },
-  {
-    name: "Rest API Backend",
-    desc: (
-      <>
-        A REST API backend built with Node.js, Express, and MongoDB. Includes
-        user authentication with JWT and bcrypt password hashing, plus full CRUD
-        for posts. You can test CRUD functionality with any API client. You can
-        test the authentication using the{" "}
-        <a
-          href="https://intro-backend-steel.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-cyan-500 underline hover:text-cyan-400"
-        >
-          simple frontend form I created
-        </a>
-        .
-      </>
-    ),
-    img: backend,
-    liveLink: "https://intro-backend-steel.vercel.app/",
-    githubLink: "https://github.com/NoahRod108/intro-backend",
-    tags: ["API", "REST", "Express.js", "Node.js", "MongoDB", "JWT"],
-  },
+  // {
+  //   name: "Rest API Backend",
+  //   desc: (
+  //     <>
+  //       A REST API backend built with Node.js, Express, and MongoDB. Includes
+  //       user authentication with JWT and bcrypt password hashing, plus full CRUD
+  //       for posts. You can test CRUD functionality with any API client. You can
+  //       test the authentication using the{" "}
+  //       <a
+  //         href="https://intro-backend-steel.vercel.app/"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //         className="text-cyan-500 underline hover:text-cyan-400"
+  //       >
+  //         simple frontend form I created
+  //       </a>
+  //       .
+  //     </>
+  //   ),
+  //   img: backend,
+  //   liveLink: "https://intro-backend-steel.vercel.app/",
+  //   githubLink: "https://github.com/NoahRod108/intro-backend",
+  //   tags: ["API", "REST", "Express.js", "Node.js", "MongoDB", "JWT"],
+  // },
 ];
 
 export const navItems = [
